0cee932ce13baa55652f3d3692fae16086a0c6d0
TouhruCreative <touhru.shott@gmail.com>
2026-03-02 20:50:23 +0500

    add view

--- Основы Django/TestProject/testapp/views.py
+++ Основы Django/TestProject/testapp/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic import ListView
+from .models import Post
+def hello_view(request):
+    return HttpResponse("Hello world")
 
-# Create your views here.
+def example_view(request):
+    return HttpResponse(f"""
+    {request.method}
+    {request.GET.get("name")}
+    {request.POST.get("title")}
+    {request.user}
+    {request.session}
+""")
+
+def post_create(requst):
+    if requst.method == "POST":
+        title=requst.POST.get("title")
+        return HttpResponse("Created")
+    
+def post_list(request):
+    context = {
+        "title": "My posting"
+    }
+    return render(request, "post_list.html",context=context)
+
+def example(reqest):
+    return redirect("post_list")
+
+def detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    context={"post":post}
+    return render(request, "detail.html",context=context )
+
+class MyView(View):
+    def get(self,request):
+        return HttpResponse("get response")
+    def post(self,reqest):
+        return HttpResponse("post response")
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post_list.html"
+    context_object_name="posts"
